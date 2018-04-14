f6f89507ee9b8b8e466433313e9ae1db346ec0ef
Jivesh <jrt274@cornell.edu>
2018-04-14 15:00:33 -0400

    print to console on emit

--- server.js
+++ server.js
@@ -34,9 +34,11 @@ serial.pipe(parser);
 parser.on('data', function(data) { // on data from the arduino
   if(data=='rst'){  // if its the 'rst' string call reset
     io.emit('reset');
+    console.log('reset');
   }else{ // any other data we try to forward by spliting it
     var transmitData = [data.split(',')[0],data.split(',')[1]];
     io.emit('new-pos', transmitData);
+    console.log('new-pos');
   }
 });
 //----------------------------------------------------------------------------//
